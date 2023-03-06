2a3cf3277787d3efa05e3dc35c06057475f59332
Zachary <41703755+zknepp1@users.noreply.github.com>
2023-03-06 14:01:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
 	version='1.0',
 	author='Zachary Knepp',
 	author_email='zachary.m.knepp-1@ou.edu',
-	packages=find_packages(urllib, PyPDF2,urllib.request, os, requests,bs4,
-                        re, urllib.parse, sqlite3,io, numpy),
+	packages=find_packages(urllib, PyPDF2, urllib.request, os, requests, bs4,
+                        re, urllib.parse, sqlite3, io, numpy),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
 )
